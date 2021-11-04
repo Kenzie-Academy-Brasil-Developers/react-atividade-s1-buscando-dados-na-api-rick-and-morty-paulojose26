3f25986244e8f6983d0d1ce0ea7920b1353974d7
PauloJose26 <paulojoliveira.2616@gmail.com>
2021-11-03 21:24:23 -0300

    Pequenos ajustes

--- src/components/CharCard/index.jsx
+++ src/components/CharCard/index.jsx
@@ -3,7 +3,7 @@ import "./style.css";
 const CharCard = ({ char }) => {
     return (
         <div key={ char.id } className={`CharCard ${ char.status }`}>
-            <h3 className="CharCard--name">{ char.name.length < 15 ? char.name : `${char.name.slice(0, 15)}...` }</h3>
+            <h3 className="CharCard--name">{ char.name.length < 13 ? char.name : `${char.name.slice(0, 12)}...` }</h3>
             <figure className="CharCard--photo">
                 <img src={ char.image } alt={ char.name } />
             </figure>
